15e1487bf86336443235339a339f8d7a2f5677cf
Migal Vyacheslav <migal.vyacheslav@gmail.com>
2025-10-21 23:43:59 +0500

    Добавлены локаторы и структура проекта

--- tests/test_dns.py
+++
@@ -1,8 +0,0 @@
-# tests/test_dns.py
-import socket
-
-def test_dns_resolves():
-    name = "stellarburgers.education-services.ru"
-    ip = socket.gethostbyname(name)
-    print("Resolved IP for", name, "->", ip)
-    assert ip is not None and ip != ""
